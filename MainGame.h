49d8cc4d1fe9f2a8f6b2337ade26414bb244ae7c
CatDadJynx <Andrew.crawford.440@gmail.com>
2022-02-11 11:13:34 -0500

    Add files via upload

---
+++ MainGame.h
@@ -0,0 +1,14 @@
+class Vector {
+  public:
+    float x = 0;
+    float y = 0;
+};
+
+const uint8_t screen_height = 64;
+const uint8_t screen_width = 128;
+
+const uint8_t viewport_center_height = screen_height / 2;
+const uint8_t viewport_center_width = screen_width / 2;
+
+const uint16_t world_size_x = 256;
+const uint16_t world_size_y = 128;
---
+++ MainGame.ino
@@ -0,0 +1,74 @@
+#include "MainGame.h"
+#include "GameSprites.h"
+#include <Arduboy2.h>
+Arduboy2 arduboy;
+
+Vector camera;
+Vector buildingLoc;
+
+void setup() {
+  arduboy.begin();
+  arduboy.flashlight();
+  arduboy.setFrameRate(60);
+  arduboy.clear();
+  
+  //Initialize things here
+}
+
+uint8_t frame = 0;
+
+void loop() {
+  if (!arduboy.nextFrame()) {
+    return;
+  }
+ 
+  arduboy.clear();
+ 
+  //Game code here
+  arduboy.pollButtons();
+  // Handle input
+  handleInput();
+  Sprites::drawSelfMasked(buildingLoc.x, buildingLoc.y, building0, 0 );
+  debug();
+  
+  arduboy.display();
+}
+
+void handleInput()
+{
+
+  // Move cursor and camera
+  if (arduboy.pressed(UP_BUTTON))
+  {
+      --camera.y;
+      
+      //update player
+  }
+
+  if (arduboy.pressed(DOWN_BUTTON))
+  {
+      ++camera.y;
+
+      //update player
+  }
+
+  if (arduboy.pressed(LEFT_BUTTON))
+  {
+      --camera.x;
+      
+      //update player
+}
+
+  if (arduboy.pressed(RIGHT_BUTTON))
+  {
+      ++camera.x;
+
+      //update player
+  }
+}
+
+void debug()
+{
+  arduboy.print(camera.x);
+  arduboy.print(camera.y);
+}
