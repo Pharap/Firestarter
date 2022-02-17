13949a03c5bd5772f5572a238e108490b218f092
CatDadJynx <Andrew.crawford.440@gmail.com>
2022-02-17 10:33:04 -0500

    Add files via upload

--- MainGame.ino
+++ MainGame.ino
@@ -6,10 +6,17 @@ Arduboy2 arduboy;
 Vector camera;
 Vector buildingLoc;
 
+const uint8_t world_width = 256;
+const uint8_t world_height = 256;
+
+// A 2D array of tile types/tile indices
+uint8_t world[world_height][world_width] { /* Fill the map data in as necessary */ };
+
 void setup() {
   arduboy.begin();
   arduboy.clear();
-  
+  arduboy.initRandomSeed();
+  uint8_t tileType = random (0,3);
   //Initialize things here
 }
 
@@ -26,7 +33,8 @@ void loop() {
   arduboy.pollButtons();
   // Handle input
   handleInput();
-  Sprites::drawSelfMasked(buildingLoc.x - camera.x, buildingLoc.y - camera.y, building0, 0);
+  drawMap();
+  //Sprites::drawSelfMasked(buildingLoc.x - camera.x, buildingLoc.y - camera.y, buildingPlaceholders, tileType);
   debug();
   
   arduboy.display();
@@ -62,6 +70,29 @@ void handleInput()
       ++camera.x;
 
       //update player
+  }
+}
+
+void drawMap()
+{
+  for(uint8_t y = 0; y < world_height; ++y)
+  {
+    // Calculate the y position to draw the tile at, 6 is tile height
+    int16_t drawY = ((y * 6) - camera.y);
+    
+    for(uint8_t x = 0; x < world_width; ++x)
+    {
+      // Calculate the x position to draw the tile at, 6 is tile width:
+      int16_t drawX = ((x * 6) - camera.x);
+      
+      uint8_t tileType = world[y][x];
+      
+      // Assuming that your tile types are the same as
+      // the frames used in your tilesheet. Otherwise
+      // you'll need a way to determine the sprite index
+      // from the tile type.
+      Sprites::drawOverwrite(drawX, drawY, buildingPlaceholders, tileType);
+    }
   }
 }
 
