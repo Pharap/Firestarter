5a636053f03ab1560eae8b8afdf469d58925b7ab
CatDadJynx <Andrew.crawford.440@gmail.com>
2022-02-23 03:45:04 -0500

    Add files via upload

--- MainGame.h
+++ MainGame.h
@@ -1,8 +1,15 @@
+#include "TileType.h"
+#include <Arduboy2.h>
+Arduboy2 arduboy;
+#include "GameSprites.h"
+
 class Vector {
   public:
     float x = 0;
     float y = 0;
 };
+
+Vector camera;
 
 //Appropriate data types?
 const uint8_t screen_height = 64;
@@ -11,6 +18,45 @@ const uint8_t screen_width = 128;
 const uint8_t viewport_center_height = screen_height / 2;
 const uint8_t viewport_center_width = screen_width / 2;
 
+const uint8_t world_width = 256;
+const uint8_t world_height = 256;
+const uint8_t mapHeight = 9;
+const uint8_t mapWidth = 9;
+
+// A 2D array of tile types/tile indices
+uint8_t world[world_height][world_width] { /* Fill the map data in as necessary */ };
+TileType tileMap[mapHeight][mapWidth] {};
+
 //World array?
 
 //Coordinates to isomentric?
+
+void generateMap()
+{
+  for(uint8_t tileY = 0; tileY < world_height; ++tileY)
+    for(uint8_t tileX = 0; tileX < world_width; ++tileX)
+      tileMap[tileY][tileX] = static_cast<TileType>(random() % 4);
+}
+
+void drawMap()
+{
+  for(uint8_t y = 0; y < world_height; ++y)
+  {
+    // Calculate the y position to draw the tile at, 6 is tile height
+    int16_t drawY = ((y * 6) - camera.y);
+    
+    for(uint8_t x = 0; x < world_width; ++x)
+    {
+      // Calculate the x position to draw the tile at, 6 is tile width:
+      int16_t drawX = ((x * 6) - camera.x);
+      
+      uint8_t tileType = world[y][x];
+      
+      // Assuming that your tile types are the same as
+      // the frames used in your tilesheet. Otherwise
+      // you'll need a way to determine the sprite index
+      // from the tile type.
+      Sprites::drawOverwrite(drawX, drawY, buildingPlaceholders, tileType);
+    }
+  }
+}
--- MainGame.ino
+++ MainGame.ino
@@ -1,26 +1,12 @@
 #include "MainGame.h"
-#include "TileType.h"
-#include "GameSprites.h"
-#include <Arduboy2.h>
-Arduboy2 arduboy;
 
-Vector camera;
 Vector buildingLoc;
-
-const uint8_t world_width = 256;
-const uint8_t world_height = 256;
-const uint8_t mapHeight = 9;
-const uint8_t mapWidth = 9;
-
-// A 2D array of tile types/tile indices
-uint8_t world[world_height][world_width] { /* Fill the map data in as necessary */ };
-TileType tileMap[mapHeight][mapWidth] {};
 
 void setup() {
   arduboy.begin();
   arduboy.clear();
   arduboy.initRandomSeed();
-  uint8_t tileType = random (0,3);
+  //uint8_t tileType = random (0,3);
   //Initialize things here
   generateMap();
 }
@@ -75,36 +61,6 @@ void handleInput()
       ++camera.x;
 
       //update player
-  }
-}
-
-void generateMap()
-{
-  for(uint8_t tileY = 0; tileY < world_height; ++tileY)
-    for(uint8_t tileX = 0; tileX < world_width; ++tileX)
-      tileMap[tileY][tileX] = static_cast<TileType>(random() % 4);
-}
-
-void drawMap()
-{
-  for(uint8_t y = 0; y < world_height; ++y)
-  {
-    // Calculate the y position to draw the tile at, 6 is tile height
-    int16_t drawY = ((y * 6) - camera.y);
-    
-    for(uint8_t x = 0; x < world_width; ++x)
-    {
-      // Calculate the x position to draw the tile at, 6 is tile width:
-      int16_t drawX = ((x * 6) - camera.x);
-      
-      uint8_t tileType = world[y][x];
-      
-      // Assuming that your tile types are the same as
-      // the frames used in your tilesheet. Otherwise
-      // you'll need a way to determine the sprite index
-      // from the tile type.
-      Sprites::drawOverwrite(drawX, drawY, buildingPlaceholders, tileType);
-    }
   }
 }
 
